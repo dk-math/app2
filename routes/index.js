0ac57ebba8ba9081111ebea96dfdd8f15e64d2b0
dk-math <dk.gianluigi.1978@gmail.com>
2023-01-08 15:52:38 +0900

    header sign css

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const data = {
+    title: "映画GO"
+  }
+  res.render('index', data);
 });
 
 module.exports = router;
