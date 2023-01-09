3ccab02f9384e8ee337e46b2ae21587bca6ed658
dk-math <dk.gianluigi.1978@gmail.com>
2023-01-09 18:22:17 +0900

    TMDB API background

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,83 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
 
-/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   const apiKey = process.env.TMDB_API_KEY;
+//   const url1 = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=ja&region=JP&page=1`;
+//   const url2 = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=ja&resion=JP&page=2`;
+//   const movieList = [];
+//   fetch(url1)
+//   .then(res => {
+//     return res.json();
+//   })
+//   .then(result => {
+//     movieList[0] = result.results;
+//     return fetch(url2);
+//   })
+//   .then(res => {
+//     return res.json();
+//   })
+//   .then(result => {
+//     movieList[1] = result.results;
+//     const movieData = movieList[0].concat(movieList[1]);
+//     const data = {
+//       title: "映画GO",
+//       movies: movieData,
+//     };
+//     res.render('index', data)
+//   });
+// });
+
 router.get('/', function(req, res, next) {
-  const data = {
-    title: "映画GO"
-  }
-  res.render('index', data);
+  const apiKey = process.env.TMDB_API_KEY;
+  const url1 = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=ja&region=JP&page=1`;
+  const url2 = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=ja&resion=JP&page=2`;
+  const url3 = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=ja&resion=JP&page=3`;
+  const url4 = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=ja&resion=JP&page=4`;
+  const url5 = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=ja&resion=JP&page=5`;
+  let movieList;
+  fetch(url1)
+  .then(res => {
+    return res.json();
+  })
+  .then(result => {
+    movieList = result.results;
+    return fetch(url2);
+  })
+  .then(res => {
+    return res.json();
+  })
+  .then(result => {
+    movieList = movieList.concat(result.results);
+    return fetch(url3);
+  })
+  .then(res => {
+    return res.json();
+  })
+  .then(result => {
+    movieList = movieList.concat(result.results);
+    return fetch(url4);
+  })
+  .then(res => {
+    return res.json();
+  })
+  .then(result => {
+    movieList = movieList.concat(result.results);
+    return fetch(url5);
+  })
+  .then(res => {
+    return res.json();
+  })
+  .then(result => {
+    movieList = movieList.concat(result.results);
+    const data = {
+      title: "映画GO",
+      movies: movieList,
+    };
+    res.render('index', data)
+  });
 });
 
 module.exports = router;
