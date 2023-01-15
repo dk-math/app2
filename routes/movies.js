fcdf2251376408961a98a87427a78025da0ac6e9
dk-math <dk.gianluigi.1978@gmail.com>
2023-01-15 12:53:56 +0900

    feat: Add hover effect

--- routes/movies.js
+++ routes/movies.js
@@ -6,8 +6,7 @@ require('dotenv').config();
 router.get('/' , (req, res, next) => {
     let keyword = req.query.keyword;
     const movieApiKey = process.env.TMDB_API_KEY;
-    const fontawesomeApiKey = process.env.FONTAWESOME_API_KEY;
-    const url = `https://api.themoviedb.org/3/search/movie/?api_key=${movieApiKey}&language=ja&region=JP&query=${keyword}&page=1`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${movieApiKey}&language=ja&region=JP&query=${keyword}&page=1`;
     fetch(url)
       .then(res => {
         return res.json();
@@ -18,10 +17,9 @@ router.get('/' , (req, res, next) => {
         const data = {
           title: "映画GO",
           movies: result.results,
-          keyword: keyword,
-          fontawesomeApiKey: fontawesomeApiKey
+          keyword: keyword
         }
-        res.render('movies', data);
+        res.render('movies/movies', data);
       })
       .catch(err => {
         console.log(err);
@@ -29,8 +27,25 @@ router.get('/' , (req, res, next) => {
   
   });
 
-  router.get('/detail', (req, res, next) => {
-
+  router.get('/detail/:id', (req, res, next) => {
+    let id = req.params.id;
+    const movieApiKey = process.env.TMDB_API_KEY;
+    const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${movieApiKey}&language=ja`;
+    fetch(url)
+      .then(res => {
+        return res.json();
+      })
+      .then(result => {
+        const data = {
+          title: "映画GO",
+          movie: result,
+        }
+        console.log(data.movie);
+        res.render('movies/detail', data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   })
 
   module.exports = router;
